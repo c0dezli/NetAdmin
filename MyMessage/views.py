7daacbef37ab2bf8a866fb372664415b53e3194d
SteveLeeLX <steveleelx@gmail.com>
2015-08-28 23:21:46 -0400

    redirect url fixed

--- MyMessage/views.py
+++ MyMessage/views.py
@@ -9,6 +9,7 @@ from .models import Message, Conversation
 
 def message_detail_view(request, conversation_id):
     conversation = Conversation.objects.get(id=conversation_id)
+    con_type = conversation.type
     message_list = conversation.messages.all()
     for message in message_list:
         if message.unread and request.user.username == message.receiver:
@@ -19,9 +20,11 @@ def message_detail_view(request, conversation_id):
     return render(request, 'message_detail.html',
                   {'message_list': message_list,
                    'receiver': conversation.receiver,
-                   'conversation_id': conversation_id})
+                   'conversation_id': conversation_id,
+                   'con_type': con_type,
+                   'redirect_url': request.get_full_path()})
 
-def send_message_view(request, type):
+def send_message_view(request):
     '''
     POST: reciver, sender, content, redirect_url, (title), (reply_to)
     :param redirect_url:
@@ -31,13 +34,14 @@ def send_message_view(request, type):
         # new conversation
         try:
             conversation_id = request.POST['conversation_id']
+            con_type = request.POST['con_type']
             # init a new conversation instance
             new_conversation = Conversation()
             new_conversation.title = request.POST['title']
             new_conversation.p1 = request.POST['sender']
             new_conversation.p2 = request.POST['receiver']
             new_conversation.starter = request.POST['sender']
-            new_conversation.type = type
+            new_conversation.type = con_type
             new_conversation.save()
             # init a new message instance
             new_message = Message()
@@ -49,7 +53,7 @@ def send_message_view(request, type):
             new_conversation.messages.add(new_message)
             # todo: notifications
             # return to /****/****/(conversation_id)/
-            return HttpResponse('send success')
+            return HttpResponseRedirect(request.POST['redirect_url'])
         # replying a exist conversation
         except:
             conversation_id = request.POST['conversation_id']
@@ -65,7 +69,7 @@ def send_message_view(request, type):
             conversation.messages.add(new_message)
             # todo: notifications
             # return to /****/****/(conversation_id)/
-            return HttpResponse('send success')
+            return HttpResponseRedirect(request.POST['redirect_url'])
     else:
         return HttpResponseRedirect('/index/')
 
